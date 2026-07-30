43ca99e1789d030947726f0dbf12382964d4df44
yogeshwtp <yogeshwtp@gmail.com>
2026-07-30 13:06:14 +0530

    trying again

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        // Write your code here
+        System.out.println("Welcome to Java Track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now I am learning how to express that logic using Java");
     }
 }
