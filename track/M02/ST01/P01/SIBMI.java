32ebf1c48c02ca65c047b59a0cb10dbcba2a1446
yogeshwtp <yogeshwtp@gmail.com>
2026-08-03 15:57:43 +0530

    practice problem: BMI,SI,total again

--- track/M02/ST01/P01/SIBMI.java
+++ track/M02/ST01/P01/SIBMI.java
@@ -1,3 +1,28 @@
 public class SIBMI {
-    
+    public static void main(String[] args) {
+
+        double principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+
+        int mark1 = 78;
+        int mark2 = 84;
+        int mark3 = 69;
+        int mark4 = 91;
+        int mark5 = 88;
+
+        double simpleInterest = principal * rate * time / 100.0;
+        double totalAmount = principal + simpleInterest;
+        double bmi = weight / (height * height);
+        int totalMarks = mark1 + mark2 + mark3 + mark4 + mark5;
+        double percentage = totalMarks * 100.0 / 500;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
+    }
 }
