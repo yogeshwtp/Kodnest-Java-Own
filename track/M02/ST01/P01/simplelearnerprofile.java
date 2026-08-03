23216a5b39d3e32fa8ff61987c85457d32e1f134
yogeshwtp <yogeshwtp@gmail.com>
2026-08-03 15:52:01 +0530

    practice problem: BMI,SI,total

---
+++ track/M02/ST01/P01/simplelearnerprofile.java
@@ -0,0 +1,14 @@
+public class simplelearnerprofile {
+    public static void main(String[] args) {
+
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
