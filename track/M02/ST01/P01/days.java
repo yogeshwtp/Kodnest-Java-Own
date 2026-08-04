21f488b41a9ee8bfc0756af38ca3b704ffa164d2
yogeshwtp <yogeshwtp@gmail.com>
2026-08-04 11:33:04 +0530

    print and println -- learn

---
+++ track/M02/ST01/P01/days.java
@@ -0,0 +1,12 @@
+public class days{
+    public static void main(String[] args)
+    {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
