9142a4f330e096cddf7795855251e143fcc05f4b
yogeshwtp <yogeshwtp@gmail.com>
2026-08-04 12:29:19 +0530

    Pos Neg 0 check

---
+++ track/M02/ST01/P01/PosNeg.java
@@ -0,0 +1,12 @@
+public class PosNeg {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("Positive");//25
+        } else if (num < 0) {
+            System.out.println("Negative");//-25
+        } else {
+            System.out.println("Zero");//0
+        }
+    }
+}
