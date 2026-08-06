de7e86a09fee86b0ca8f6438be3c10da3eb43571
yogeshwtp <yogeshwtp@gmail.com>
2026-08-06 12:28:02 +0530

    break and continue - learn

---
+++ track/M02/T02/breaklearn.java
@@ -0,0 +1,12 @@
+package T02;
+
+public class breaklearn {
+    public static void main(String[] args){
+        for(int i=1;i<=10; i++){
+            System.out.println(i);
+            if(i==6){
+            break;
+        }
+        }
+    }
+}
