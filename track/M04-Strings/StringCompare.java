d0a177d642ee4e3355f1189252bdafa6b614eca2
Yogesh <yogeshwtp@gmail.com>
2026-09-08 16:50:49 +0530

    Strings-learn

---
+++ track/M04-Strings/StringCompare.java
@@ -0,0 +1,17 @@
+public class StringCompare{
+    public static void main(String[] args) {
+     String s1 = "Java";
+     String s2 = "JaVa";
+     if(s1== s2){
+        System.out.println("Reference are same");
+     }else{
+        System.out.println("Reference are different");
+     }
+
+     if(s1.equalsIgnoreCase(s2)){
+        System.out.println("Strings are same");  
+     }else{
+        System.out.println("Content are different");
+     }
+    }
+}
